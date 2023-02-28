11d497ad0d8c6521f6cfa2e84d3d8e6d40eec1d1
unknown <unk1222@gmail.com>
2023-02-28 12:39:54 -0500

    Finished 404 page

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -3,7 +3,20 @@ import { Fragment } from "react";
 const PageNotFound = () => {
   return (
     <Fragment>
-      <div></div>
+      <div className="min-h-screen banner bg-indigo-50 flex flex-col justify-center items-center">
+        <h1 className="text-blue-600 font-bold text-6xl lg:text-[240px] text-center mb-8">
+          404
+        </h1>
+        <p className="text-center font-bold text-4xl mb-4">
+          This page is not available at this time.
+        </p>
+        <a
+          href="/home"
+          className="btn-primary bg-blue-600 hover:bg-blue-700 text-white border-blue-600 hover:border-blue-700"
+        >
+          Return Home
+        </a>
+      </div>
     </Fragment>
   );
 };
